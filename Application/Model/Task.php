a4fa8e8dc213e262ae221e3b3ac6ce421ed8a86a
banhtieu <phuongjolly@Les-MacBook-Air.local>
2015-05-25 15:29:14 +0700

    - update detail for model class

--- Application/Model/Task.php
+++ Application/Model/Task.php
@@ -1,10 +1,26 @@
+<?php
+
 namespace Application\Model;
 use Core\Database\Model;
 
 class Task extends Model {
 
+    /**
+     * @var int id - the unique id of this object
+     * @column(identity)
+     */
     public $id;
+
+    /**
+     * @var string the name
+     * @column(string)
+     */
     public $name;
+
+    /**
+     * @var string the description
+     * @column(string)
+     */
     public $description;
 
 }
